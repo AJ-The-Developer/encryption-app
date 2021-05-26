24d76035c6f0b990ff5bef121e666df33999a2cc
Neil Foxcroft <neilfoxcroftpro@gmail.com>
2021-05-26 16:31:01 +0200

    vernam cipher and text input
    added the base cipher for vernam and started with the user input frontend

--- src/app/components/vernam/vernam.component.ts
+++ src/app/components/vernam/vernam.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { VernamService } from 'src/app/services/vernam.service';
 
 @Component({
   selector: 'app-vernam',
   templateUrl: './vernam.component.html',
   styleUrls: ['./vernam.component.scss']
 })
+
 export class VernamComponent implements OnInit {
+  inputtext = 'thisistheteststring';
+  keytext = 'mykey';
+  outputtext = '';
+  
+  vernamService;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(vernamService: VernamService) {
+    this.vernamService = vernamService;
   }
 
+  ngOnInit(): void {}
+
+  encryptload() {
+    this.vernamService.vernam(this.inputtext, this.keytext);
+    this.outputtext = this.vernamService.vernam(this.inputtext, this.keytext);
+    console.log(this.inputtext);
+    console.log(this.keytext);
+    console.log(this.outputtext);
+  }
+
+  decryptload() {
+    this.vernamService.vernam(this.outputtext, this.keytext);
+    this.outputtext = this.vernamService.vernam(this.outputtext, this.keytext);
+    console.log(this.inputtext);
+    console.log(this.keytext);
+    console.log(this.outputtext);
+  }
 }
--- src/app/services/vernam.service.ts
+++ src/app/services/vernam.service.ts
@@ -3,7 +3,28 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class VernamService {
+  plaintext: String = "Enterprise-wide incremental Graphic Interface";
+  key: String = "jan";
 
   constructor() { }
+
+  vernam(input, key) {
+   
+    let l_key = key.length;
+    let fromCharCode = String.fromCharCode;
+
+    let inputstream = input.toCharArray();
+    let cipherstream = [inputstream.lenght];
+    console.log(inputstream, cipherstream);
+    
+    /*
+    return input.replace(/[\s\S]/g, function(c, i) {
+      return fromCharCode(key.charCodeAt(i % l_key) ^ c.charCodeAt(0));
+    });
+    */
+
+    
+  };
 }
